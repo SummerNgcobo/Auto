0c1a25b68870c7019ab0285ae1809a82863ff3f8
Summer <summerngcobo02@gmail.com>
2025-05-09 11:53:23 +0200

    ManageCustomer bug fixed.

--- Documents/NetBeansProjects/Inventory Management System/src/Home.java
+++ Documents/NetBeansProjects/Inventory Management System/src/Home.java
@@ -132,6 +132,7 @@ public class Home extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        new ManageCustomer().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
